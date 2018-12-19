5ecfc92c1939880635683247a1964811e2a92fa8
Jason Nelson <Jasonnelson11@gmail.com>
2018-12-18 23:45:55 -0500

    Finally working, Final Version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,12 +11,26 @@ var crysNumG;
 //Saying to start when a user clicks something
 document.onkeyup = function(event) {
 
+
  //Setting the random numbers   
 ran = Math.floor(Math.random() * 120) + 19;
 crysNumR = Math.floor(Math.random() * 12) + 1;
 crysNumB = Math.floor(Math.random() * 12) + 1;
 crysNumY = Math.floor(Math.random() * 12) + 1;
 crysNumG = Math.floor(Math.random() * 12) + 1;
+
+function reset(){
+    ran = Math.floor(Math.random() * 120) + 19;
+    crysNumR = Math.floor(Math.random() * 12) + 1;
+    crysNumB = Math.floor(Math.random() * 12) + 1;
+    crysNumY = Math.floor(Math.random() * 12) + 1;
+    crysNumG = Math.floor(Math.random() * 12) + 1;
+    totalScore = 0;
+    $("#random").text(ran);
+    $("#totalScore").text(totalScore);
+    $("#Wins").text(countWin);
+    $("#Losses").text(countLoss);
+}
 
 //Increases total score and put the 
 $( "#Red-Crystal" ).click(function() {
@@ -27,12 +41,10 @@ $( "#Red-Crystal" ).click(function() {
     if(totalScore === ran){
         countWin++;
         $("#Wins").text(countWin);
-        alert("You Win")
     }
     if(totalScore > ran){
         countLoss++;
       $("#Losses").text(countLoss);
-      alert("You Lost")
     }
 });
 
@@ -44,12 +56,10 @@ $( "#Blue-Crystal" ).click(function() {
     if(totalScore === ran){
         countWin++;
         $("#Wins").text(countWin);
-        alert("You Win")
     }
     if(totalScore > ran){
         countLoss++;
       $("#Losses").text(countLoss);
-      alert("You Lost")
     }
   });
 
@@ -60,14 +70,15 @@ $( "#Blue-Crystal" ).click(function() {
     $("#totalScore").text(totalScore);
 
     if(totalScore === ran){
+        reset();
         countWin++;
         $("#Wins").text(countWin);
-        alert("You Win")
+    
     }
     if(totalScore > ran){
+        reset();
         countLoss++;
       $("#Losses").text(countLoss);
-      alert("You Lost")
     }
 });
 
@@ -77,16 +88,35 @@ $( "#Green-Crystal" ).click(function() {
     console.log("totalscore: " + totalScore)
     $("#totalScore").text(totalScore);
     if(totalScore === ran){
+        reset();
         countWin++;
         $("#Wins").text(countWin);
-        alert("You Win")
+      
     }
     if(totalScore > ran){
+        reset();
         countLoss++;
       $("#Losses").text(countLoss);
-      alert("You Lost")
+      
     }
 });
+
+if(totalScore === ran){
+  reset(); 
+   countWin++;
+   totalScore = 0;
+
+}
+if(totalScore > ran){
+    reset();
+    countLoss++;
+    totalScore = 0;
+
+}
+$("#random").text(ran);
+$("#totalScore").text(totalScore);
+$("#Wins").text(countWin);
+$("#Losses").text(countLoss);
 
 
 }
